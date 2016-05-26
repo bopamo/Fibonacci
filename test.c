f72b37141be31a3fcbb1769e81557f8cfdbd6c52
Bopamo Osaisai <bopamo@users.noreply.github.com>
2016-05-25 19:19:48 -0700

    Update test.c

--- test.c
+++ test.c
@@ -1,3 +1,4 @@
+#include <CppUTest/CommandLineTestRunner.h>
 #include <CppUTest/TestHarness.h>
 
 TEST_GROUP(FirstTestGroup)
@@ -7,4 +8,10 @@ TEST_GROUP(FirstTestGroup)
 TEST(FirstTestGroup, FirstTest)
 {
    FAIL("Fail me!");
+}
+
+/* Tests begin Here */
+int main(int ac, char** av)
+{
+    return CommandLineTestRunner::RunAllTests(ac, av);
 }
