685118d1d09d01d59f98cd667a4e42fea9615cdf
Bopamo Osaisai <bopamo@users.noreply.github.com>
2016-05-25 18:53:20 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Environment: `uname -a`"
-echo "Compiler: `$CC --version`"
+echo "C   Compiler: `$CC --version`"
+echo "C++ Compiler: `$CPP --version`"
 
 make || exit 1
 make test || exit 1
